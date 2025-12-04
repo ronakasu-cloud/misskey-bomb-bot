5ada862b60d088c96e3d235a153f931abe0ac3ce
ronakasu-cloud <ronakasu@gmail.com>
2025-12-04 12:21:09 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from misskey import Misskey
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 # ======================
 # Misskey設定
@@ -10,14 +10,18 @@ INSTANCE_URL = "https://misskey.stream"       # インスタンスURL
 mi = Misskey(INSTANCE_URL, i=TOKEN)
 
 def post_message():
-    now = datetime.now()
+    # 日本時間（JST）に変換
+    JST = timezone(timedelta(hours=9))
+    now = datetime.now(JST)
     hour = now.hour
+
     if hour == 0:
         text = "起爆"
     else:
         text = "爆破" * hour
+
     mi.notes_create(text=text)
     print(f"{hour}時に投稿: {text}")
 
-# GitHub Actionsで呼ばれるので、スケジューラは不要
+# GitHub Actionsで呼ばれるのでスケジューラは不要
 post_message()
