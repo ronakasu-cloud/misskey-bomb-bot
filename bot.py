51b8e2eac207d7c67804b1b67063c8ae13d9a830
ronakasu-cloud <ronakasu@gmail.com>
2025-12-04 11:51:13 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 from misskey import Misskey
-import os
 
-# GitHub Actions の秘密変数からトークン取得
-token = os.getenv("MISSKEY_TOKEN")
+# ★トークン直書き版（あなたのトークン）
+TOKEN = "F7l2B04qPnBI1dxFycZU4MvWRIIoNqEI"
 
-# インスタンスURL（正しい綴り）
-mi = Misskey("https://misskey.stream")
+# インスタンスURL（スペル注意）
+mi = Misskey("https://misskey.stream", i=TOKEN)
 
-# ログイン
-mi.login(token=token)
-
-# 投稿内容
+# 投稿内容（固定文：爆破）
 mi.notes_create(text="爆破")
